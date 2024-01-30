8f3a10a34f6d4033c667cbc108a8063f6c2bc6d0
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-01-30 17:28:25 +0100

    Testing FizzBuzz function

--- src/intro.js
+++ src/intro.js
@@ -2,9 +2,9 @@
 export const max = (a, b) => (a > b ? a : b);
 
 // Exercise
-export function fizzBuzz(n) {
-  if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz';
-  if (n % 3 === 0) return 'Fizz';
-  if (n % 5 === 0) return 'Buzz';
-  return n.toString();
-}
+export const fizzBuzz = (n) => {
+	if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz';
+	if (n % 3 === 0) return 'Fizz';
+	if (n % 5 === 0) return 'Buzz';
+	return n.toString();
+};
--- tests/intro.test.js
+++ tests/intro.test.js
@@ -1,5 +1,5 @@
 import { it, expect, describe } from 'vitest';
-import { max } from '../src/intro';
+import { fizzBuzz, max } from '../src/intro';
 
 describe('max', () => {
 	it('should return the first argument if it is greater', () => {
@@ -34,5 +34,20 @@ describe('max', () => {
 
 		// Assert: Verify TV is off
 		expect(result).toBe(1);
+	});
+});
+
+describe('fizzBuzz', () => {
+	it('should return FizzBuzz if argument is divisible by 3 AND 5', () => {
+		expect(fizzBuzz(30)).toBe('FizzBuzz');
+	});
+	it('should return Fizz if argument is only divisible by 3', () => {
+		expect(fizzBuzz(9)).toBe('Fizz');
+	});
+	it('should return Buzz if argument is only divisible by 5', () => {
+		expect(fizzBuzz(125)).toBe('Buzz');
+	});
+	it('should return arg as a string if it is no divisible by 3 or 5', () => {
+		expect(fizzBuzz(4)).toBe('4');
 	});
 });
