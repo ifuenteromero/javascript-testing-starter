3ec0eb1c0c60039243c054489406a1d9be9ec09c
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-02-01 12:09:21 +0100

    Positive and negative testing. Testing calculateDiscount

--- tests/core.test.js
+++ tests/core.test.js
@@ -1,5 +1,5 @@
 import { it, expect, describe } from 'vitest';
-import { getCoupons } from '../src/core';
+import { calculateDiscount, getCoupons } from '../src/core';
 
 describe('getCoupons', () => {
 	const coupons = getCoupons();
@@ -22,5 +22,26 @@ describe('getCoupons', () => {
 			expect(coupon.discount).toBeGreaterThanOrEqual(0);
 			expect(coupon.discount).toBeLessThanOrEqual(1);
 		});
+	});
+});
+
+describe('calculateDiscount', () => {
+	it('should return discounted price if given valid code', () => {
+		expect(calculateDiscount(10, 'SAVE10')).toBe(9);
+		expect(calculateDiscount(10, 'SAVE20')).toBe(8);
+	});
+	it('should handle non-numeric price', () => {
+		expect(calculateDiscount('10', 'SAVE10')).toMatch(/invalid/i);
+		expect(calculateDiscount('10', 'INVALIDCODE')).toMatch(/invalid/i);
+	});
+	it('should handle non-string discount code', () => {
+		expect(calculateDiscount(10, 10)).toMatch(/invalid/i);
+		expect(calculateDiscount('10', 1)).toMatch(/invalid/i);
+	});
+	it('should handle negative price', () => {
+		expect(calculateDiscount(-10, 'SAVE10')).toMatch(/invalid/i);
+	});
+	it('should handle invalid discount code', () => {
+		expect(calculateDiscount(10, '')).toBe(10);
 	});
 });
