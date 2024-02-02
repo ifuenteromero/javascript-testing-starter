1fc26c0f6f265dd4fd4f7facf8b3fcc2f413fd8d
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-02-02 19:08:28 +0100

    exercise testing submitOrder

--- tests/mocking.test.js
+++ tests/mocking.test.js
@@ -3,14 +3,17 @@ import {
 	getPriceInCurrency,
 	getShippingInfo,
 	renderPage,
+	submitOrder,
 } from '../src/mocking';
 import { getExchangeRate } from '../src/libs/currency';
 import { getShippingQuote } from '../src/libs/shipping';
 import { trackPageView } from '../src/libs/analytics';
+import { charge } from '../src/libs/payment';
 
 vi.mock('../src/libs/currency');
 vi.mock('../src/libs/shipping');
 vi.mock('../src/libs/analytics');
+vi.mock('../src/libs/payment');
 
 // A mock function is a function that imitates the behaviour of a real function
 // We use them to test a unit in asolation
@@ -95,5 +98,30 @@ describe('renderPage', () => {
 	it('should call analytics', async () => {
 		await renderPage();
 		expect(trackPageView).toHaveBeenCalledWith('/home');
+	});
+});
+
+describe('submitOrder', () => {
+	const order = { totalAmount: 10 };
+	const creditCard = { creditCardNumber: '1234' };
+
+	it('should charge the customer', async () => {
+		vi.mocked(charge).mockResolvedValue({ status: 'success' });
+		await submitOrder(order, creditCard);
+		expect(charge).toBeCalledWith(creditCard, order.totalAmount);
+	});
+
+	it('should return success when payment is successful', async () => {
+		vi.mocked(charge).mockResolvedValue({
+			status: 'success',
+		});
+		const result = await submitOrder(order, creditCard);
+		expect(result).toEqual({ success: true });
+	});
+
+	it('should return failed when payment is failed', async () => {
+		vi.mocked(charge).mockResolvedValue({ status: 'failed' });
+		const result = await submitOrder(order, creditCard);
+		expect(result).toEqual({ success: false, error: 'payment_error' });
 	});
 });
