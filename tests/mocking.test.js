a9f3446c5fc560d2fbb9e77cb9c65b0e6eed510c
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-02-02 17:12:49 +0100

    2 Creating mock functions

---
+++ tests/mocking.test.js
@@ -0,0 +1,39 @@
+import { vi, it, expect, describe } from 'vitest';
+
+// A mock function is a function that imitates the behaviour of a real function
+// We use them to test a unit in asolation
+// Si tenemos una functionA que utiliza una functionB. Para testear la functionA in asolation
+// we have to decouple A from functionB para eso usamos mocks
+// We replace the real implementation of function B with a mock function that we can control
+// in our tests
+
+describe('test suite', () => {
+	it('test case', () => {
+		const greet = vi.fn(); // esto retorna una mock function
+		// si la ejecutamos greet() por defecto retorna undefined
+		greet.mockReturnValue('Hello');
+		const result = greet(); //*1
+		console.log({ result });
+
+		const promise = vi.fn();
+		promise.mockResolvedValue('Irene');
+		promise().then((resultPromise) => console.log({ resultPromise })); //*2
+
+		///
+		const sum = vi.fn();
+		sum.mockImplementation((a) => a + 3);
+		const resultSum = sum(102); // 3
+		console.log({ resultSum });
+
+		//
+		expect(greet).toHaveBeenCalled(); //*1
+		expect(promise).toHaveBeenCalled(); //*2
+		expect(sum).toHaveBeenCalled(); // 3
+
+		expect(sum).toHaveBeenCalledWith(102);
+
+		expect(greet).toHaveBeenCalledOnce(); //*1
+		expect(promise).toHaveBeenCalledOnce(); //*2
+		expect(sum).toHaveBeenCalledOnce(); // 3
+	});
+});
