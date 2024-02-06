b0913631db0fede2176edc873b31675f8ef6f950
Irene Fuente Romero <ifuenteromero@gmail.com>
2024-02-06 16:31:52 +0100

    Add ts tests

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+export const calculateDiscount = (price: number, discountCode: string) => {
+	if (price <= 0) {
+		return 'Invalid price';
+	}
+
+	let discount = 0;
+	if (discountCode === 'SAVE10') {
+		discount = 0.1;
+	} else if (discountCode === 'SAVE20') {
+		discount = 0.2;
+	}
+
+	return price - price * discount;
+};
