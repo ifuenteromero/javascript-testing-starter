d168e9710afd40f20ded11932283401661b5aaba
Irene Fuente Romero <ifuenteromero@gmail.com>
2025-12-03 07:54:39 +0100

    Exercise: Test factorial

--- src/intro2.ts
+++ src/intro2.ts
@@ -4,3 +4,10 @@ export const calculateAverage = (numbers: number[]): number => {
   const sum = numbers.reduce((total, current) => total + current, 0);
   return sum / countNumbers;
 };
+
+export const factorial = (n: number): number | undefined => {
+  if (!Number.isInteger(n)) return undefined;
+  if (n < 0) return undefined;
+  if (n === 0 || n === 1) return 1;
+  return n * (factorial(n - 1) as number);
+};
--- tests/intro2.test.ts
+++ tests/intro2.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { calculateAverage } from "../src/intro2";
+import { calculateAverage, factorial } from "../src/intro2";
 
 describe("calculateAverage", () => {
   it("should return NaN if given an empty array", () => {
@@ -16,5 +16,32 @@ describe("calculateAverage", () => {
   });
   it("should calculate the average of an array with four non-integer elements", () => {
     expect(calculateAverage([6.2, 5.6, 9.1, 3.3])).toBe(6.05);
+  });
+});
+
+describe("factorial", () => {
+  it("should return 1 if given 0", () => {
+    expect(factorial(0)).toBe(1);
+  });
+  it("should return 1 if given 1", () => {
+    expect(factorial(1)).toBe(1);
+  });
+  it("should return 2 if given 2", () => {
+    expect(factorial(2)).toBe(2);
+  });
+  it("should return 6 if given 3", () => {
+    expect(factorial(3)).toBe(6);
+  });
+  it("should return undefined if given a negative number", () => {
+    expect(factorial(-1)).toBeUndefined();
+  });
+  it("should return undefined if given a non-integer number", () => {
+    expect(factorial(1.2)).toBeUndefined();
+  });
+  it("should return 24 if given number is 4", () => {
+    expect(factorial(4)).toBe(24);
+  });
+  it("should return 3_628_800 if given number is 10", () => {
+    expect(factorial(10)).toBe(3_628_800);
   });
 });
